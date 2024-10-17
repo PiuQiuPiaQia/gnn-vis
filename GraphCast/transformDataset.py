196365806557c10f7b678d959306704b8ecc9a79
Mario <329988300@qq.com>
2024-10-17 15:17:47 +0800

    修改名称

--- GraphCast/transformDataset.py
+++ GraphCast/transformDataset.py
@@ -1,20 +1,31 @@
-from torch_geometric.transforms import  AddLaplacianEigenvectorPE, AddRandomWalkPE
+from torch_geometric.transforms import AddLaplacianEigenvectorPE, AddRandomWalkPE
+from torch_geometric.data import Data  # 确保导入Data类
 from era52Geometric import loadEra5Dateset
 
-# AddLaplacianEigenvectorPE: https://pytorch-geometric.readthedocs.io/en/latest/generated/torch_geometric.transforms.AddLaplacianEigenvectorPE.html?highlight=transform+ran
+# 加载ERA5数据集
+G, data = loadEra5Dateset()
 
-G,data = loadEra5Dateset()
-
+# 显示原始数据
 print(data)
 
-# 假设 data 是一个 PyTorch Geometric Data 对象
+# 使用Laplacian Eigenvector PE转换
 transform = AddLaplacianEigenvectorPE(k=3)
-data_transformed = transform(data)
+data_transformed_Laplacian = transform(data)
 
-print(data_transformed)
+# 显示转换后的数据
+print(data_transformed_Laplacian)
 
-# 假设 data 是一个 PyTorch Geometric Data 对象
+# 使用Random Walk PE转换
 transform2 = AddRandomWalkPE(walk_length=30)
-data_transformed2 = transform2(data)
+data_transformed_Random = transform2(data)
 
-print(data_transformed2)
+# 显示第二次转换后的数据
+print(data_transformed_Random)
+
+# 保存转换后的数据到文件系统
+data_transformed_Laplacian.save('data_transformed_Laplacian.pt')
+data_transformed_Random.save('data_transformed_Random.pt')
+
+# 之后你可以使用Data类的load方法重新加载这些数据
+# new_data_transformed = Data.load('data_transformed_Laplacian.pt')
+# new_data_transformed2 = Data.load('data_transformed_Random.pt')
